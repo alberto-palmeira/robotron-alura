587f685ccc93b3975f58c4d783311f5de3724103
Alberto <jalberto.palmeira@gmail.com>
2022-12-24 14:28:49 -0300

    Atualização da funcionalidade de edição do Robotron, de maneira que, incluir peças, seus status são alterados.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,62 @@
-function manipulaDados (operacao, controle) {
-    const peca = controle.querySelector('[data-contador]');
+function customizaRobotron (operacao, controle, peca) {
+    const quantidadePeca = controle.querySelector('[data-contador]');
     
-    if (operacao === '+') {
-        peca.value = parseInt(peca.value) + 1;
-    } else if (operacao === '-') {
-        peca.value = parseInt(peca.value) - 1;
-    };
+        if (operacao === '+') {
+            estatisticas.forEach( (elemento) => {
+                elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+            });
+            quantidadePeca.value = parseInt(quantidadePeca.value) + 1;
+        } else if (operacao === '-' && quantidadePeca.value > 0) {
+            estatisticas.forEach((elemento) => {
+                elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+            })
+            quantidadePeca.value = parseInt(quantidadePeca.value) - 1;
+        };
+    }
+
+const botao = document.querySelectorAll('.controle-ajuste');
+const estatisticas = document.querySelectorAll('[data-estatistica]');
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
 }
 
-const controle = document.querySelectorAll('.controle-ajuste');
-
-controle.forEach(function(elemento){ 
+botao.forEach(function(elemento){ 
     elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        const operacao = evento.target.dataset.controle;
+        const controle = evento.target.parentNode;
+        const peca = evento.target.dataset.peca;
+
+        customizaRobotron(operacao, controle, peca);
     });
 });
